65dd89f44423de7e44fefa854bad7bf6a38f4ab2
Chris Pliakas <cpliakas@gmail.com>
2017-09-16 21:01:57 -0400

    Updated comment

--- main.go
+++ main.go
@@ -10,7 +10,8 @@ func main() {
 }
 
 // helloWorld calls the hello_world() function defined in the HelloWorld.c
-// file, converts the character array to a native Go string, and prints it.
+// file, converts the character array that is returned to a native Go
+// string, and writes it to stdout.
 func helloWorld() {
 	ptr := C.hello_world()
 	fmt.Println(C.GoString(ptr))
